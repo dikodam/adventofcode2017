ba253aafbf45f4c1c2d855ff4eed1c8a8d4b69d5
adiko <dikodam@googlemail.com>
2017-12-01 09:01:22 +0100

    removed unneccesary sum stream :'(

--- src/main/java/de/dikodam/adventofcode/task1/Task1.java
+++ src/main/java/de/dikodam/adventofcode/task1/Task1.java
@@ -1,6 +1,5 @@
 package de.dikodam.adventofcode.task1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -29,37 +28,30 @@ public class Task1 {
     }
 
     private void subtask2(List<Integer> ints) {
-        List<Integer> summands = new ArrayList<>();
         int listSize = ints.size();
-
+        int sum = 0;
         for (int i = 0; i < listSize; i++) {
             Integer n0 = ints.get(i);
             Integer n1 = ints.get((i + listSize / 2) % listSize);
             if (Objects.equals(n0, n1)) {
-                summands.add(n0);
+                sum += n0;
             }
         }
 
-        System.out.println("Sum of subtask2: " + sumInts(summands));
+        System.out.println("Sum of subtask2: " + sum);
     }
 
     private void subtask1(List<Integer> ints) {
-        List<Integer> summands = new ArrayList<>();
+        int sum = 0;
         int listSize = ints.size();
         for (int i = 0; i < listSize; i++) {
             Integer n0 = ints.get(i);
             Integer n1 = ints.get((i + 1) % listSize);
             if (Objects.equals(n0, n1)) {
-                summands.add(n0);
+                sum += n0;
             }
         }
-        System.out.println("Sum of subtask1: " + sumInts(summands));
-    }
-
-    private int sumInts(List<Integer> summands) {
-        return summands.stream()
-            .mapToInt(Integer::intValue)
-            .sum();
+        System.out.println("Sum of subtask1: " + sum);
     }
 
     public List<Integer> parseStringToInts(String inputString) {
