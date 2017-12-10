64d44bcf8464f814d3d810aefbe7b6f28252a038
Adam <dikodam@googlemail.com>
2017-12-10 16:55:13 +0100

    day9 template

---
+++ src/main/java/de/dikodam/adventofcode/day09/Day09.java
@@ -0,0 +1,22 @@
+package de.dikodam.adventofcode.day09;
+
+import de.dikodam.adventofcode.tools.AbstractDay;
+
+public class Day09 extends AbstractDay {
+
+    public static void main(String[] args) {
+        Day09 day09 = new Day09();
+        day09.task1();
+        day09.task2();
+    }
+
+    @Override
+    public void task1() {
+
+    }
+
+    @Override
+    public void task2() {
+
+    }
+}
