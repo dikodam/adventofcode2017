a01d1985dc7873830a959fb6f252b6609ccf9c4c
adiko <dikodam@googlemail.com>
2017-12-11 11:13:14 +0100

    day10 task 1 started

--- src/main/java/de/dikodam/adventofcode/day10/Day10.java
+++ src/main/java/de/dikodam/adventofcode/day10/Day10.java
@@ -2,7 +2,11 @@ package de.dikodam.adventofcode.day10;
 
 import de.dikodam.adventofcode.tools.AbstractDay;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Day10 extends AbstractDay {
 
@@ -12,10 +16,56 @@ public class Day10 extends AbstractDay {
         day10.task2();
     }
 
+    List<Integer> input;
+
+    public Day10() {
+        String[] splitInputString = getInput(this.getClass().getSimpleName())
+            .get(0)
+            .split(",");
+        input = Arrays.stream(splitInputString)
+            .map(Integer::parseInt)
+            .collect(toList());
+    }
+
     @Override
     public void task1() {
-        List<String> input = getInput(this.getClass().getSimpleName());
+        int currentPosition = 0;
+        int skipSize = 0;
+        int[] list = IntStream.range(0, 256).toArray();
+
+        for (int length : input) {
+            int[] sublist = getSubList(list, currentPosition, length);
+            sublist = reverse(sublist);
+            write(sublist, list, currentPosition);
+            currentPosition += length + skipSize;
+            skipSize++;
+        }
+
+        // TODO print smth
+
         System.out.println("Task 1: ");
+    }
+
+    private void write(int[] sublist, int[] targetList, int currentPosition) {
+        for (int i = 0; i < sublist.length; i++) {
+            int targetPosition = (i + currentPosition) % targetList.length;
+            targetList[targetPosition] = sublist[i];
+        }
+    }
+
+    private int[] reverse(int[] sublist) {
+        int[] newSublist = new int[sublist.length];
+        // TODO reverse
+        return newSublist;
+    }
+
+    private int[] getSubList(int[] list, int fromInclusive, int lenght) {
+        int[] sublist = new int[lenght];
+
+        for (int i = fromInclusive; i < fromInclusive + lenght; i++) {
+            // TODO iterate wrapping modulo
+        }
+        return sublist;
     }
 
     @Override
