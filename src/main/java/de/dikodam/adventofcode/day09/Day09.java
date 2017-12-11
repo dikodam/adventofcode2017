e517d1a993f430fc317db58685d2510405e3c945
adiko <dikodam@googlemail.com>
2017-12-11 10:30:24 +0100

    day9 taks 1 + 2

--- src/main/java/de/dikodam/adventofcode/day09/Day09.java
+++ src/main/java/de/dikodam/adventofcode/day09/Day09.java
@@ -2,6 +2,9 @@ package de.dikodam.adventofcode.day09;
 
 import de.dikodam.adventofcode.tools.AbstractDay;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Day09 extends AbstractDay {
 
     public static void main(String[] args) {
@@ -10,13 +13,47 @@ public class Day09 extends AbstractDay {
         day09.task2();
     }
 
+    private char[] input;
+
+    public Day09() {
+        input = getInput(getClass().getSimpleName()).get(0).toCharArray();
+    }
+
     @Override
     public void task1() {
+        boolean garbageState = false;
+        int score = 0;
+        int nonCancelledGarbageCount = 0;
+        Deque<Character> stack = new ArrayDeque<>();
 
+        for (int i = 0; i < input.length; i++) {
+            char inputChar = input[i];
+            if (garbageState) {
+                if (inputChar == '!') {
+                    i++;
+                } else if (inputChar == '>') {
+                    garbageState = false;
+                } else {
+                    nonCancelledGarbageCount++;
+                }
+            } else {
+                if (inputChar == '{') {
+                    stack.push(inputChar);
+                } else if (inputChar == '}') {
+                    score += stack.size();
+                    stack.pop();
+                } else if (inputChar == '<') {
+                    garbageState = true;
+                }
+            }
+        }
+
+        System.out.println("Task 1: Score is " + score);
+        System.out.println("Task 2: Count of non-cancelled garbage is " + nonCancelledGarbageCount);
     }
 
     @Override
     public void task2() {
-
+        // implemented in task1
     }
 }
