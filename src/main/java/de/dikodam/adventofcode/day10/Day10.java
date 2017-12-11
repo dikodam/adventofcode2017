b5de1feb117d8f96c95a7373efd3c8142e31288f
adiko <dikodam@googlemail.com>
2017-12-11 10:32:19 +0100

    day10 template

---
+++ src/main/java/de/dikodam/adventofcode/day10/Day10.java
@@ -0,0 +1,25 @@
+package de.dikodam.adventofcode.day10;
+
+import de.dikodam.adventofcode.tools.AbstractDay;
+
+import java.util.List;
+
+public class Day10 extends AbstractDay {
+
+    public static void main(String[] args) {
+        Day10 day10 = new Day10();
+        day10.task1();
+        day10.task2();
+    }
+
+    @Override
+    public void task1() {
+        List<String> input = getInput(this.getClass().getSimpleName());
+        System.out.println("Task 1: ");
+    }
+
+    @Override
+    public void task2() {
+        System.out.println("Task 2: ");
+    }
+}
